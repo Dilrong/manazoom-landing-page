52d71ad5a3c9d82bdfc4617ff65554cc707974f8
dilrong <wet932@naver.com>
2020-05-26 21:42:53 +0900

    Update: color

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -3,8 +3,8 @@ export const COLORS = {
   lightGray: "#F3F5F6",
   mediumGray: "#B0B8BA",
   gray: "#A0A4A6",
-  babyBlue: "#50CAF9",
-  blue: "#5368f1",
+  babyBlue: "#00F2FE",
+  blue: "#4FACFE",
 }
 
 export const GRADIENT = `linear-gradient(to top right, ${COLORS.babyBlue}, ${
